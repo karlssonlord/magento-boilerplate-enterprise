16be656c34be044f3425a2e1c99df70127ffbb9b
Erik Eng <mail@ptz0n.se>
2013-11-13 12:22:34 +0100

    Patched KL_Page module to add static resource checksum.

--- app/code/community/KL/Page/Block/Html/Head.php
+++ app/code/community/KL/Page/Block/Html/Head.php
@@ -19,6 +19,8 @@ class KL_Page_Block_Html_Head extends Mage_Page_Block_Html_Head
      */
     public function getCssJsHtml()
     {
+        if (empty($this->_data['items'])) return '';
+
         // separate items by types
         $lines  = array();
         foreach ($this->_data['items'] as $item) {
@@ -76,5 +78,80 @@ class KL_Page_Block_Html_Head extends Mage_Page_Block_Html_Head
             }
         }
         return $html;
+    }
+
+    /**
+     * Merge static and skin files of the same format into 1 set of HEAD directives or even into 1 directive
+     *
+     * Will attempt to merge into 1 directive, if merging callback is provided. In this case it will generate
+     * filenames, rather than render urls.
+     * The merger callback is responsible for checking whether files exist, merging them and giving result URL
+     *
+     * @param string $format - HTML element format for sprintf('<element src="%s"%s />', $src, $params)
+     * @param array $staticItems - array of relative names of static items to be grabbed from js/ folder
+     * @param array $skinItems - array of relative names of skin items to be found in skins according to design config
+     * @param callback $mergeCallback
+     * @return string
+     */
+    protected function &_prepareStaticAndSkinElements($format, array $staticItems, array $skinItems, $mergeCallback = null)
+    {
+        $designPackage = Mage::getDesign();
+        $baseJsUrl = Mage::getBaseUrl('js');
+        $items = array();
+        if ($mergeCallback && !is_callable($mergeCallback)) {
+            $mergeCallback = null;
+        }
+
+        // get static files from the js folder, no need in lookups
+        foreach ($staticItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $path = Mage::getBaseDir() . DS . 'js' . DS . $name;
+                $items[$params][] = $mergeCallback ? $path
+                    : $baseJsUrl . $name . $this->_fileSum($path);
+            }
+        }
+
+        // lookup each file basing on current theme configuration
+        foreach ($skinItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $path = $designPackage->getFilename($name, array('_type' => 'skin'));
+                $items[$params][] = $mergeCallback ? $path
+                    : $designPackage->getSkinUrl($name, array()) . $this->_fileSum($path);
+            }
+        }
+
+        $html = '';
+        foreach ($items as $params => $rows) {
+            // attempt to merge
+            $mergedUrl = false;
+            if ($mergeCallback) {
+                $mergedUrl = call_user_func($mergeCallback, $rows);
+            }
+            // render elements
+            $params = trim($params);
+            $params = $params ? ' ' . $params : '';
+            if ($mergedUrl) {
+                $html .= sprintf($format, $mergedUrl, $params);
+            } else {
+                foreach ($rows as $src) {
+                    $html .= sprintf($format, $src, $params);
+                }
+            }
+        }
+        return $html;
+    }
+
+    /**
+     * Calculate File Sum
+     *
+     */
+    private function _fileSum($file)
+    {
+        $sum = '';
+        if(file_exists($file)) {
+            $sum = '?'.md5_file($file);
+        }
+
+        return $sum;
     }
 }
